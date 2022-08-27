7776f6d772312c4474dd00e7434a8fa011bcf125
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-08-27 08:52:51 +0600

    load data with api and daynamic set users

--- 4-1-fetch.js
+++ 4-1-fetch.js
@@ -4,8 +4,27 @@
 
 
 
-function loadData() {
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
+// function loadData() {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(response => response.json())
+//         .then(Data => Addbutton(Data))
+// }
+
+
+
+
+
+function loadData2() {
+    fetch('https://jsonplaceholder.typicode.com/users') //load this url in JSON placeholder site with users 
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(Data => Addbutton(Data))
+}
+
+
+function Addbutton(Data) {
+
+
+    console.log(Data);
+
+
 }
---
+++ 5-index.js
@@ -0,0 +1,18 @@
+function loadUsers2() {
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(DataArray => users2(DataArray))
+}
+
+function users2(DataArray) {
+    const ul = document.getElementById('user-list');
+    for (const User of DataArray) {
+        const nameuser = User.name;
+        const li = document.createElement('li');
+        li.innerText = nameuser;
+        ul.appendChild(li);
+    }
+
+
+}
